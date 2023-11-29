afbf8b6ad81bea2e44eae528c0bb947787f9296b
Yaroslav2777 <y.negoda@istu.edu.ua>
2023-11-29 11:26:42 +0200

    Singleton.cpp

---
+++ Singleton.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+class Singleton {
+public:
+    // Метод доступу до єдиного екземпляру класу
+    static Singleton& getInstance() {
+        static Singleton instance; // Єдиний екземпляр класу
+        return instance;
+    }
+
+    // Деякі методи чи дані класу
+    void showMessage() {
+        cout << "Hi Singleton!" << endl;
+    }
+
+    // Позбавляємося можливості копіювання та присвоєння
+    Singleton(const Singleton&) = delete;
+    Singleton& operator=(const Singleton&) = delete;
+
+private:
+    // Забороняємо створення зовнішніми методами
+    Singleton() {}
+};
+
+// Приклад використання Singleton
+int main() {
+    // Отримання єдиного екземпляру
+    Singleton& singleton = Singleton::getInstance();
+
+    // Використання методу
+    singleton.showMessage();
+
+    return 0;
+}
