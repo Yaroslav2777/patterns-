c349f622141c294ed11eb28dad49e6258759534b
Yaroslav2777 <y.negoda@istu.edu.ua>
2023-12-20 10:49:07 +0200

    Bridgee.cpp

---
+++ Bridgee.cpp
@@ -0,0 +1,43 @@
+﻿﻿#include <iostream>
+
+class Vehicle {
+protected:
+    SpeedControl* speedControl;
+
+public:
+    Vehicle(SpeedControl* control) : speedControl(control) {}
+
+    virtual void drive() = 0;
+    virtual void refuel() = 0;
+    virtual void measureSpeed() {
+        speedControl->measureSpeed();
+    }
+
+    virtual ~Vehicle() {}
+};
+
+class Car : public Vehicle {
+public:
+    Car(SpeedControl* control) : Vehicle(control) {}
+
+    void drive() override {
+        // Реалізація для автомобіля
+    }
+
+    void refuel() override {
+        // Реалізація заправки автомобіля
+    }
+};
+
+class Motorcycle : public Vehicle {
+public:
+    Motorcycle(SpeedControl* control) : Vehicle(control) {}
+
+    void drive() override {
+        // Реалізація для мотоцикла
+    }
+
+    void refuel() override {
+        // Реалізація заправки мотоцикла
+    }
+};
